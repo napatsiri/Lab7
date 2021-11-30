815bc3c981714bc4e56770b5f883fabeaa5d1be0
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-11-30 17:17:51 +0700

    lab7_4.cpp completed

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -5,11 +5,21 @@ using namespace std;
 
 int main(){
 	
-	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
-	int id[] = {123,196,225,329,420,451,515,666,690,787};
-	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
+	int N=10;
+	string name[N] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
+	int id[N] = {123,196,225,329,420,451,515,666,690,787};
+	double gpa[N] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
-		
+	double grade;
+    cout << "Please input GPA: ";
+	cin >> grade;
+
+	int i=0;
+	while(i<N){
+		if(gpa[i]>=grade){
+			cout << id[i] << " " <<  name[i] << " " << gpa[i] << "\n";
+		}
+		i++;
+	}
 	return 0;
 }
