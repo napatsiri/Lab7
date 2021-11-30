59885655f3f076c93faa50989fe2bb6f4a4235df
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-11-30 17:31:19 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+  
+  while (a>360)
+  {
+    a = a-360;
+  }
+  while (b>360)
+  {
+    b = b-360;
+  }
+
+  int c = a-b;
+  if(c<0)
+  {
+    c = -c;
+  }else{
+    c = c;
+  }
+
+  while (c >= 360)
+  {
+    c = c-360;
+  }
+  if(c > 180)
+  {
+    c = 360-c;
+  }
+  return c;
+}
+  
 
 int main(){
   cout << adiff(180,270);
@@ -13,3 +43,4 @@ int main(){
   cout << adiff(1000,280);
   cout << adiff(60,244);
 }
+
